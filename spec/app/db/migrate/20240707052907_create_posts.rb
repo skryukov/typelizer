7f2f109857be3c82b0b2f5cb70da704811b046db
Andrey Novikov <envek@envek.name>
2024-11-19 21:21:52 +0900

    Define underlying type for enums to be always string

--- spec/app/db/migrate/20240707052907_create_posts.rb
+++ spec/app/db/migrate/20240707052907_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :category
+      t.integer :category
       t.text :body
       t.datetime :published_at
       t.references :user, null: false, foreign_key: true
