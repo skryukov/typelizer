c947a311032b7a54774b4955c01d559ed9f65d8c
Svyatoslav Kryukov <me@skryukov.dev>
2025-02-28 19:00:40 +0300

    Add test cases for AMS and OjSerializers

--- spec/app/app/serializers/ams/user_serializer.rb
+++ spec/app/app/serializers/ams/user_serializer.rb
@@ -1,13 +1,18 @@
 module Ams
   class UserSerializer < BaseSerializer
     typelize_from ::User
-    attributes :id, :username, :active
+    attributes :id, :username, :active, :name
 
     has_one :invitor, serializer: UserSerializer
 
     has_many :posts, serializer: PostSerializer
 
     typelize id: [:string, nullable: true]
+
+    typelize :string
+    attribute :first_name do |object|
+      object.username.split(" ").first
+    end
 
     class FooSerializer < UserSerializer
       typelize_from ::User
--- spec/app/app/serializers/oj_serializers/user_serializer.rb
+++ spec/app/app/serializers/oj_serializers/user_serializer.rb
@@ -1,11 +1,16 @@
 module OjSerializers
   class UserSerializer < BaseSerializer
     typelize_from ::User
-    attributes :id, :username, :active
+    attributes :id, :username, :active, :name
 
     has_one :invitor, serializer: UserSerializer
 
     has_many :posts, serializer: PostSerializer
+
+    typelize :string
+    attribute :first_name do |object|
+      object.username.split(" ").first
+    end
 
     class FooSerializer < UserSerializer
       typelize_from ::User
