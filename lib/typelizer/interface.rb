d066e95474d5af522ee186093a204c727ed69fdf
Svyatoslav Kryukov <me@skryukov.dev>
2026-03-26 12:28:41 +0300

    Add filename_mapper config
    Co-authored-by: Ryan Workman <19210117+rdavid1099@users.noreply.github.com>

--- lib/typelizer/interface.rb
+++ lib/typelizer/interface.rb
@@ -36,7 +36,11 @@ module Typelizer
     end
 
     def filename
-      name.gsub("::", "/")
+      if config.filename_mapper
+        config.filename_mapper.call(config.serializer_name_mapper.call(serializer))
+      else
+        name.gsub("::", "/")
+      end
     end
 
     def index_path(index_dir)
